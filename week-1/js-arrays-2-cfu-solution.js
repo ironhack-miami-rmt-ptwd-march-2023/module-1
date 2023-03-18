d6dd0907888186001215b3ef0062e20677798000
sandrabosk <sandraboskovic@hotmail.com>
2023-03-18 21:00:44 +0100

    add cfu arrays 2 solution

---
+++ week-1/js-arrays-2-cfu-solution.js
@@ -0,0 +1,23 @@
+const fruits = ['apple', 'plum', 'strawberries'];
+
+// 1
+for(let i=0; i < fruits.length; i++){
+  console.log(fruits[i]);
+}
+
+// apple
+// plum
+// strawberries
+
+// 2
+
+fruits.forEach(function(oneFruit, index){
+    console.log(index, oneFruit);
+});
+
+// ES6:
+fruits.forEach((oneFruit, index) => console.log(index, oneFruit));
+
+// 0 'apple'
+// 1 'plum'
+// 2 'strawberries'
