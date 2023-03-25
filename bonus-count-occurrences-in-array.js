dacc4a218a918b23a6c6af8589bbc3639a2ffe78
sandrabosk <sandraboskovic@hotmail.com>
2023-03-25 19:36:53 +0100

    add another verion of a solution to the bonus

--- bonus-count-occurrences-in-array.js
+++ bonus-count-occurrences-in-array.js
@@ -16,21 +16,29 @@ const wordsCount = [
   function countWords(arr) {
 
     // create an empty object to store the word counts
-    const counts = {};
+    const count = {};
+
+    // for (let i = 0; i < array.length; i++) {
+    //   if (count[array[i]] === undefined){
+    //     count[array[i]] = 1
+    //   } else {
+    //     count[array[i]] +=1 
+    //   }
+    // }
   
     // loop through the array of words and increment the count for each word
     for (let word of arr) {
 
-      if (counts[word] === undefined){ // if elem of the arr doesn't exist as a key in the object then it is undefined
-        counts[word] = 1; // this creates the "key" in the object (the keys are the elements of the array)
+      if (count[word] === undefined){ // if elem of the arr doesn't exist as a key in the object then it is undefined
+        count[word] = 1; // this creates the "key" in the object (the keys are the elements of the array)
       } else {
-        counts[word] += 1; // if the elem of the arr already exists as the key in the object, then count ++ 
+        count[word] += 1; // if the elem of the arr already exists as the key in the object, then count ++ 
       }
 
     }
   
     // return the object with the word counts
-    return counts;
+    return count;
   }
   
   console.log(countWords(wordsCount));
@@ -46,3 +54,20 @@ const wordsCount = [
 //     truth: 1,
 //     disobedience: 1
 //   }
+
+
+function countWords (array) {
+  let count = {};
+  
+
+  
+  for (let word of array){
+    if (count[word] === undefined){
+      count[word] = 1
+    } else {
+      count[word] += 1
+    }
+  }
+  
+  return count;
+}
