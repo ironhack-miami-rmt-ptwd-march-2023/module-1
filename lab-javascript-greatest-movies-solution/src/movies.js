2b77def1fa27fa89a01e8a50366d2d776edd2740
sandrabosk <sandraboskovic@hotmail.com>
2023-04-14 04:15:17 +0200

    add lab solution

---
+++ lab-javascript-greatest-movies-solution/src/movies.js
@@ -0,0 +1,160 @@
+// Iteration 1: All directors? - Get the array of all directors.
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// How could you "clean" a bit this array and make it unified (without duplicates)?
+function getAllDirectors(moviesArray) {
+      // Use `.map()` to create a new array containing only director names
+  const directorsArr = moviesArray.map((movie) => movie.director)
+
+  return directorsArr;
+
+}
+
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function howManyMovies(moviesArray) {
+    return moviesArray.filter(
+        eachMovie =>
+          eachMovie.director === 'Steven Spielberg' &&
+          eachMovie.genre.includes('Drama')
+      ).length;
+}
+
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals
+function scoresAverage(moviesArray) {
+  // return !moviesArray.length? 0 : Number((moviesArray.reduce((a,b) => b.score? a + b.score : a, 0)/moviesArray.length).toFixed(2));
+  // this is the one line version
+
+  if (!moviesArray.length) {
+    return 0;
+  }
+
+  let total = moviesArray.reduce((a, b) => {
+    if (b.score) {
+      return a + b.score;
+    } else {
+      return a;
+    }
+  }, 0);
+
+  // you can use Number(), parseInt() or simply plus +
+  return Number((total / moviesArray.length).toFixed(2));
+}
+
+// Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesScore(moviesArray) {
+    let drMovies = moviesArray.filter(eachMovie =>
+        eachMovie.genre.includes('Drama')
+      );
+      return scoresAverage(drMovies);
+}
+
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(moviesArray) {
+    let newArray = [...moviesArray];
+    // spread operator is to make sure not to mutate the original array because .sort() does change/mutate the original array so always make sure you create a safe copy before sorting
+    newArray.sort((a, b) => {
+      if (a.year > b.year) {
+        return 1;
+      } else if (b.year > a.year) {
+        return -1;
+      } else {
+        if (a.title > b.title) {
+          return 1;
+        } else if (b.title > a.title) {
+          return -1;
+        }
+        return 0;
+      }
+    });
+    return newArray;
+}
+
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(moviesArray) {
+    return [...moviesArray]
+    .sort((a, b) => {
+      if (a.title > b.title) {
+        return 1;
+      } else if (a.title < b.title) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    .map(eachMovie => eachMovie.title)
+    .slice(0, 20);
+}
+
+// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function convertDurationToNumberOfMinutes (durationAsString) {
+  const minutes = durationAsString.split(" ").reduce((accum, currVal) => {
+    
+    // console.log(`${parseInt(currVal)} ==> type: ${typeof currVal} ==> ${currVal}`)
+
+    if(currVal.includes("h")){
+      return accum + parseInt(currVal) * 60
+    } else {
+      return accum + parseInt(currVal)
+    }
+  }, 0);
+
+
+  return minutes
+};
+
+function turnHoursToMinutes(moviesArray) {
+  const updatedMoviesArray = moviesArray.map(movie => {
+      
+    // let newMovieObject = {};
+
+    // newMovieObject.title = movie.title;
+    // newMovieObject.year = movie.year;
+    // newMovieObject.director = movie.director;
+    // newMovieObject.duration = convertDurationToNumberOfMinutes(movie.duration);
+    // newMovieObject.genre = movie.genre;
+    // newMovieObject.score = movie.score;
+
+    // return newMovieObject;
+
+    return {
+      ...movie, // copy the movie object
+      duration:convertDurationToNumberOfMinutes(movie.duration) // change only the value of "duration" property
+    }
+  })
+
+return updatedMoviesArray
+}
+
+// BONUS - Iteration 8: Best yearly score average - Best yearly score average
+function bestYearAvg(moviesArray) {
+  if(!moviesArray.length){
+    return null;
+  }
+
+  const dictionary = {};
+
+  moviesArray.forEach(movie => {
+    if(!dictionary[movie.year]){
+      dictionary[movie.year] = [];
+      dictionary[movie.year].push(movie);
+    } else {
+      dictionary[movie.year].push(movie);
+    }
+  })
+
+  // return dictionary;
+
+  let bestAvgScore = 0;
+  let bestYear;
+
+  for(const year in dictionary){
+    const currentYearAverage = scoresAverage(dictionary[year]);
+
+    if(currentYearAverage > bestAvgScore){
+      bestAvgScore = currentYearAverage;
+      bestYear = year
+    }
+  }
+
+  return `The best year was ${bestYear} with an average score of ${bestAvgScore}`
+}
+
