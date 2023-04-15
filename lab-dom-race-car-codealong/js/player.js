47ad414de5e3014d40249fcf56012a469573b5f4
sandrabosk <sandraboskovic@hotmail.com>
2023-04-15 18:28:23 +0200

    add race car codealong

---
+++ lab-dom-race-car-codealong/js/player.js
@@ -0,0 +1,78 @@
+class Player {
+    constructor(gameScreen, left, top, width, height, imgSrc) {
+      this.gameScreen = gameScreen;
+      this.left = left;
+      this.top = top;
+      this.width = width;
+      this.height = height;
+      this.directionX = 0;
+      this.directionY = 0;
+
+      this.element = document.createElement("img");
+      this.element.src = imgSrc;
+
+      this.element.style.position = "absolute";
+
+      this.element.style.width = `${this.width}px`;
+      this.element.style.height = `${this.height}px`;
+      this.element.style.left = `${this.left}px`;
+      this.element.style.top = `${this.top}px`;
+
+      this.gameScreen.appendChild(this.element);
+    }
+
+
+
+    move() {
+      this.left += this.directionX;
+      this.top += this.directionY;
+
+      // sets boundary to left hand side
+      if(this.left < 30){
+        this.left = 30;
+      }
+
+      // sets boundary to top side
+      if (this.top < 10){
+        this.top = 10;
+      }
+
+    // sets boundary to right hand side
+    if(this.left + this.width > this.gameScreen.offsetWidth - 30){
+      this.left = this.gameScreen.offsetWidth - this.width - 30;
+    }
+
+    // sets boundary to down side
+    if(this.top + this.height > this.gameScreen.offsetHeight - 10){
+      this.top = this.gameScreen.offsetHeight - this.height - 10;
+    }    
+
+
+
+      this.updatePosition();
+    }
+
+    updatePosition() {
+      this.element.style.left = `${this.left}px`;
+      this.element.style.top = `${this.top}px`;
+    }
+
+    didCollide(obstacle) {
+      const playerRect = this.element.getBoundingClientRect();
+      const obstacleRect = obstacle.element.getBoundingClientRect();
+
+      if(
+        playerRect.left < obstacleRect.right &&
+        playerRect.right > obstacleRect.left && 
+        playerRect.top < obstacleRect.bottom &&
+        playerRect.bottom > obstacleRect.top
+      ){
+        console.log("COLLISION@ 💥💥💥💥💥");
+        return true
+      } else {
+        return false;
+      }
+
+    }
+
+}
---
+++ lab-dom-race-car-codealong/js/script.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game;
+
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  })
+
+  function restartGame() {
+    location.reload();
+  }
+
+  function startGame() {
+    console.log("start game");
+
+    game = new Game();
+
+    game.start();
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+
+    console.log("key: ", key);
+
+    const possibleKeystrokes = ["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"];
+
+    if(possibleKeystrokes.includes(key)){
+      event.preventDefault();
+
+      switch(key){
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+        }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+};
